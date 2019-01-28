32a070670fe7fb03498b800dc6e4840a8a4b7cba
Guillaume Ballet <gballet@gmail.com>
2019-01-27 22:22:57 -0800

    Pictures for eth 1.x presentation

---
+++ confs/eth1x0119/initial_benchmark.m
@@ -0,0 +1,6 @@
+data = [7.36844973383544e+07, 250553.94833171365; 226338.62740010378, 898.8601453035807; 380196, 3515; 21965.933378424077, 124.17044301657084]
+bar(data(2:4,:)/1000)
+set (gca, 'xticklabel', ["sha256"; "ripemd160"; "identity"])
+title("Precompile benchmarks WASM vs. native")
+xlabel("precompile")
+ylabel("avg. execution time (ms)")
